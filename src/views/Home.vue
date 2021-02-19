b4af64396a7a130f5ec8a905c8690b6d7db0688b
raihan862 <raihankabir862@gmail.com>
2021-02-19 07:50:57 +0600

    local update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,16 @@
   <div class="home">
     <div class="make-post">
       <img src="../assets/img.png" alt="" />
-      <input type="text" placeholder="Whats on your mind" />
+      <button @click="showModal = true" placeholder="Whats on your mind" />
+      <div v-if="showModal" class="modal-container">
+        <teleport to="#post-modal">
+          <div class="modal">
+            <textarea rows="4" calss="post-data"> </textarea>
+            <button>Post</button>
+            <button @click="showModal = false">Cancle</button>
+          </div>
+        </teleport>
+      </div>
     </div>
     <div v-for="post of posts" :key="post.id">
       <Post :post="post" />
@@ -17,6 +26,7 @@ import Post from "../components/Post";
 export default {
   name: "Home",
   setup() {
+    const showModal = ref(false);
     const posts = ref([]);
     const getUsers = async () => {
       axios
@@ -27,7 +37,7 @@ export default {
     };
     onMounted(getUsers);
 
-    return { posts };
+    return { posts, showModal };
   },
   components: {
     Post,
@@ -41,15 +51,20 @@ export default {
   width: 60%;
   margin: auto;
 }
+.modal {
+  position: absolute;
+  top: 0;
+}
 .make-post {
   display: flex;
   background-color: white;
   padding: 30px;
 }
+.modal-container:fo
 .make-post img {
   width: 40px;
 }
-.make-post input {
+.make-post button {
   width: 100%;
   margin-left: 20px;
   border: none;
